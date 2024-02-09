2f95db40c57e175b72a6083b49fe3f19ad430b1c
root <root@localhost>
2024-02-09 09:57:53 +0000

    OAuth tutorial finished, still need to make instructor changes

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,4 +4,8 @@ class Admin < ApplicationRecord
   #  devise :database_authenticatable, :registerable,
   #         :recoverable, :rememberable, :validatable
   devise :omniauthable, omniauth_providers: [:google_oauth2]
+  def self.from_google(email:, full_name:, uid:, avatar_url:)
+    return nil unless email =~ /@mybusiness.com\z/
+    create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
+  end
 end

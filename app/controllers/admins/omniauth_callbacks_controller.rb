86ae1c066cb9beba0786de6f9df878cfe7d1f293
markbc25MAC <markbc25@tamu.edu>
2024-02-09 12:04:46 -0600

    OAuth2.0 working, need to redirect routes

--- app/controllers/admins/omniauth_callbacks_controller.rb
+++ app/controllers/admins/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class Admins::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    admin = Admin.from_google(from_google_params)
+    admin = Admin.from_google(**from_google_params)
 
     if admin.present?
       sign_out_all_scopes
@@ -25,13 +25,16 @@ class Admins::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
 
   def from_google_params
+
     @from_google_params ||= {
       uid: auth.uid,
       email: auth.info.email,
       full_name: auth.info.name,
       avatar_url: auth.info.image
     }
+
   end
+  
 
   def auth
     @auth ||= request.env['omniauth.auth']

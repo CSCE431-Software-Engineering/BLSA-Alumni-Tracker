071ffe5a512c01c5a1a3d935be44ca80cb66d037
samdcole48 <samdcole48@gmail.com>
2024-02-12 13:03:15 -0600

    finished resolving merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_scope :admin do
     get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
     get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
+  end
+  
   resources :education_infos do
     member do
       get :delete

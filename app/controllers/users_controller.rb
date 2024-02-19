bbaeec8d534773bfb0770edf63f89aba516de7e1
samdcole48 <samdcole48@gmail.com>
2024-02-19 13:02:19 -0600

    kept session email after sign in, added user only create and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
+  before_action :set_user, only: [:show, :edit, :update, :delete, :destroy]
 
   # GET /users or /users.json
   def index
@@ -22,34 +22,35 @@ class UsersController < ApplicationController
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
+    @user.Email = session[:email]
 
     respond_to do |format|
-      if user_signed_in? && @user.email == current_user.email
-      @user.email = current_user.email
-
-        if @user.save
-          format.html { redirect_to(user_url(@user), notice: 'User was successfully created.') }
-          format.json { render(:show, status: :created, location: @user) }
-        else
-          format.html { render(:new, status: :unprocessable_entity) }
-          format.json { render(json: @user.errors, status: :unprocessable_entity) }
-        end
+      if @user.save
+        format.html { redirect_to(user_url(@user), notice: 'User was successfully created.') }
+        format.json { render(:show, status: :created, location: @user) }
       else
-        format.html { redirect_to(root_path, alert: 'You can only create a profile for your own account.') }
-        format.json { render(json: { error: 'You can only create a profile for your own account.' }, status: :unprocessable_entity) }
+        format.html { render(:new, status: :unprocessable_entity) }
+        format.json { render(json: @user.errors, status: :unprocessable_entity) }
       end
     end
   end
 
+
   # PATCH/PUT /users/1 or /users/1.json
   def update
+
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to(user_url(@user), notice: 'User was successfully updated.') }
-        format.json { render(:show, status: :ok, location: @user) }
+      if @user.Email == session[:email]
+        if @user.update(user_params)
+          format.html { redirect_to @user, notice: 'Profile was successfully updated.' }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       else
-        format.html { render(:edit, status: :unprocessable_entity) }
-        format.json { render(json: @user.errors, status: :unprocessable_entity) }
+        format.html { redirect_to @user, alert: 'You can only update your own profile.' }
+        format.json { render json: { error: 'Unauthorized' }, status: :unauthorized }
       end
     end
   end

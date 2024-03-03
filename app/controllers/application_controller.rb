30eb977572b4f00fab1fe7960f1c6690ada36460
Hayden Holt <haydenholt60485@gmail.com>
2024-03-03 03:49:08 -0600

    updated profile functionality to pass tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!
+
+  before_action :set_current_user
+  helper_method :current_user
+
+  private
+
+  def set_current_user
+    @current_user ||= User.find_by(Email: session[:email]) if session[:email].present?
+  end
+
+  def current_user
+    @current_user
+  end
 end

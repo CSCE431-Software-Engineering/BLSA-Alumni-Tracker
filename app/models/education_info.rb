0e2b89165c382a1a7c3f88a2a9394005ce8bc8a8
root <samdcole48@gmail.com>
2024-03-04 03:57:30 +0000

    fixed first time sign on error, styling, and todolist no longer displayed once completed

--- app/models/education_info.rb
+++ app/models/education_info.rb
@@ -9,4 +9,10 @@ class EducationInfo < ApplicationRecord
   validates :Grad_Year, presence: true, length: { minimum: 4 }
   validates :university_id, presence: true
   validates :Degree_Type, presence: true
+
+  #if the attribute name starts with an uppercase letter like Degree_Type, it can be confused with a constant because Ruby constants also start with uppercase letters. In this case, using self helps to clarify that Degree_Type is an instance attribute, not a constant.
+
+  def education_info_string
+    "#{self.Degree_Type} from #{self.university.University}. #{self.Semester} #{self.Grad_Year}"
+  end
 end

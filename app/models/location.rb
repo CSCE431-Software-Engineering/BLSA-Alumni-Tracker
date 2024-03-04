4ffa0bf1afec93ca0b9f4385dde55b08698006b9
root <samdcole48@gmail.com>
2024-03-04 02:56:04 +0000

    included state and country in dropwdown

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,11 @@
 class Location < ApplicationRecord
   has_many :users
-  
+
   validates :country, presence: true
   validates :state, presence: true
   validates :city, presence: true
+
+  def location_string
+    "#{city}, #{state}, #{country}"
+  end
 end

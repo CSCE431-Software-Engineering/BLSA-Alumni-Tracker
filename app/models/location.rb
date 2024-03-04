953487a355103b10ae604018215270c571d84df1
root <samdcole48@gmail.com>
2024-03-04 02:22:32 +0000

    adjusted model to account for tests

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,7 @@
 class Location < ApplicationRecord
   has_many :users
+  
+  validates :country, presence: true
+  validates :state, presence: true
+  validates :city, presence: true
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,21 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
+  it 'is not valid without a country' do
+    location = described_class.new(country: nil)
+    expect(location).not_to be_valid
+  end
 
-  describe 'validations' do
-    it 'is not valid without a country' do
-      location = described_class.new(country: nil)
-      expect(location).not_to be_valid
-    end
+  it 'is not valid without a state' do
+    location = described_class.new(state: nil)
+    expect(location).not_to be_valid
+  end
 
-    it 'is not valid without a state' do
-      location = described_class.new(state: nil)
-      expect(location).not_to be_valid
-    end
-
-    it 'is not valid without a city' do
-      location = described_class.new(city: nil)
-      expect(location).not_to be_valid
-    end
+  it 'is not valid without a city' do
+    location = described_class.new(city: nil)
+    expect(location).not_to be_valid
   end
 end

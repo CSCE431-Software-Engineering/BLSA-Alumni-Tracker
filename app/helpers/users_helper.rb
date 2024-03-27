e4f81a419a340ab8acc61015c259dbfdb218fe11
root <samdcole48@gmail.com>
2024-03-27 19:09:42 +0000

    added page to navbar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  def current_user_is_admin?
+    logged_in_user = User.find_by(Email: session[:email])
+    logged_in_user&.is_Admin
+  end
 end

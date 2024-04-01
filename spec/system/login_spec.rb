0f11d0c1f74063ad43856f3e98b8354b3357870a
lightlyflow <lightlyflow@csce431>
2024-04-01 03:52:14 +0000

    Added navigation and login tests

---
+++ spec/system/login_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+require 'system_helper'
+
+RSpec.describe('Login', type: :system) do
+  before do
+    driven_by(:rack_test)
+    Rails.application.load_seed
+    Rails.application.env_config['devise.mapping'] = Devise.mappings[:user]
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      provider: 'google_oauth2',
+      uid: '123456789',
+      info: {
+        name: 'Pauline Wade',
+        email: 'csce431@gmail.com'
+      },
+      credentials: {
+        token: 'token',
+        refresh_token: 'refresh token'
+      }
+    }
+                                                                      )
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+  end
+
+  def set_tamu_email
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      provider: 'google_oauth2',
+      uid: '123456789',
+      info: {
+        name: 'Pauline Wade',
+        email: 'csce431@tamu.edu'
+      },
+      credentials: {
+        token: 'token',
+        refresh_token: 'refresh token'
+      }
+    }
+                                                                      )
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+  end
+
+  it '(Sunny Day) Login with Non-TAMU email' do
+    visit(root_path)
+    click_button('Sign in with Google')
+
+    expect(page).to(have_content('Successfully authenticated from Google account.'))
+  end
+
+  it '(Rainy Day) Login with TAMU email' do
+    set_tamu_email
+
+    visit(root_path)
+    click_button('Sign in with Google')
+
+    expect(page).to(have_content('Please use a personal email address. You will lose access to your tamu email address over time.'))
+  end
+
+  it '(Sunny Day) Logout' do
+    visit(root_path)
+    click_button('Sign in with Google')
+
+    click_on('Sign Out')
+    expect(page).to(have_content('Signed out successfully.'))
+  end
+end
---
+++ spec/system/navigation_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+require 'system_helper'
+
+RSpec.describe('Navigation', type: :system) do
+  before do
+    driven_by(:rack_test)
+    Rails.application.load_seed
+    Rails.application.env_config['devise.mapping'] = Devise.mappings[:user]
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+  end
+
+  it '(Sunny Day) All navigation links are present' do
+    visit(root_path)
+
+    expect(page).to(have_content('Dashboard'))
+    expect(page).to(have_content('My Profile'))
+    expect(page).to(have_content('My Education Information'))
+    expect(page).to(have_content('Sign Out'))
+  end
+
+  it '' do
+    # TODO :: Add search tests
+  end
+end
